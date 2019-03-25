ed236f3719b4aeb2e09642a6a50f042551e9ab66
Wagner Bitencourt Alves <wagner.bitencourt@hotmail.com.br>
2019-03-25 10:41:32 -0300

    Init

--- dist/schemas/userSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-    name: { type: String },
-    age: { type: String }
-});
-exports.default = UserSchema;
--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-let port = process.env.PORT || '3040';
-app_1.default.app.listen(port, function () {
-    console.log(`server running in" + ${port}`);
-});
-process.once('SIGUSR2', () => app_1.default.closedataBaseConnection('nodemon restart', () => process.kill(process.pid, 'SIGUSR2')));
-process.once('SIGINT', () => app_1.default.closedataBaseConnection('connection crashed', () => process.exit(0)));
